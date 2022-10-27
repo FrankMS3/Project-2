757141c22d8ff74af2c91c9031c60e2067646b87
Mohammed Hamdouna <hamdouna@Hamdounas-MBP.lan>
2022-10-27 19:12:16 +1100

    Adding updated indian_cities.sql file

--- indian_cities.sql
+++ indian_cities.sql
@@ -1,3 +1,4 @@
+-- Create Table indian_population with the columns below:
 DROP TABLE IF EXISTS indian_population;
 CREATE TABLE indian_population (
 City TEXT PRIMARY KEY,
@@ -5,17 +6,19 @@ Population TEXT,
 State TEXT
 );
 
-
+-- Create Table indian_pollution with the columns below:
 DROP TABLE IF EXISTS indian_pollution;
-CREATE TABLE indian_polluation (
+CREATE TABLE indian_pollution (
 State TEXT,
 City TEXT PRIMARY KEY,
 Average_ppb FLOAT
 );
 
-SELECT * FROM indian_population;
 SELECT * FROM indian_pollution;
+SELECT * FROM indian_population;
 
+
+-- Create a join between indian_population and indian_pollution on "city" using the columns below:
 SELECT indian_population.city, indian_population.population, indian_population.state, indian_pollution.average_ppb
 FROM indian_population
 JOIN indian_pollution ON
